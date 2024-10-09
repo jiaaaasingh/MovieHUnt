c5e8ba845b72dc571aacb26b141f8286063907d2
jiaaaasingh <165475098+jiaaaasingh@users.noreply.github.com>
2024-10-09 20:15:16 +0530

    addedDarkLightMode

--- script.js
+++ script.js
@@ -1,33 +1,19 @@
+let modeBtn = document.querySelector(".mode");
+let currMode = "light";
+let body = document.querySelector("body");
 
-document.addEventListener("DOMContentLoaded", function() {
-  // Create a datalist element
-  const dataList = document.createElement('datalist');
-  dataList.id = 'movie-options';
-
-  // List of movie options
-  const movies = [
-      'P.K',
-      'Bird Box',
-      'YOU',
-      'Ramaiya Vastavaiya',
-      'Inception',
-      'Haseen Dilruba',
-      'Money Heist',
-      'Stranger Things'
-  ];
-
-  // Create option elements for each movie and append them to the datalist
-  movies.forEach(movie => {
-      const option = document.createElement('option');
-      option.value = movie;
-      dataList.appendChild(option);
-  });
-
-  // Append the datalist to the body
-  document.body.appendChild(dataList);
-
-  // Reference the datalist from the search input
-  const searchInput = document.querySelector('.search-input');
-  searchInput.setAttribute('list', 'movie-options');
+modeBtn.addEventListener("click", ()=>{
+  if(currMode === "light"){
+    currMode = "dark";
+    body.classList.add("black");
+    body.classList.remove("light");
+  }
+  else{
+    currMode = "light";
+    body.classList.add("light");
+    body.classList.remove("black");
+  }
+  console.log(currMode);
 });
+
 
